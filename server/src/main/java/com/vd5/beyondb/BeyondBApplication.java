bbc9904634dc6b07d58086c05ae290459bfe37a0
yogurt05j <yogurt05j@gmail.com>
2023-03-14 09:29:33 +0900

    🎉 begin : [SV] initial commit

---
+++ server/src/main/java/com/vd5/beyondb/BeyondBApplication.java
@@ -0,0 +1,13 @@
+package com.vd5.beyondb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeyondBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeyondBApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/com/vd5/beyondb/BeyondBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vd5.beyondb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeyondBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
