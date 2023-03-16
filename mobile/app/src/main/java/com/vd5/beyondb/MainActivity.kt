64c3673f0fcd6d3d26779f5041a5cf70830672e1
johan kong <johankkong@gmail.com>
2023-03-17 00:42:24 +0900

    ✨ feat : [MB] add bluetooth permissions

--- mobile/app/src/main/java/com/vd5/beyondb/MainActivity.kt
+++ mobile/app/src/main/java/com/vd5/beyondb/MainActivity.kt
@@ -1,8 +1,16 @@
 package com.vd5.beyondb
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -12,9 +20,23 @@ import com.vd5.beyondb.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Bluetooth 관련 코드
+            // 권한 요청하기
+        ActivityCompat.requestPermissions(this, PERMISSIONS_BT, REQUEST_ALL_PERMISSION)
+            // bluetooth가 꺼져있는 경우 켜기
+        if (bluetoothAdapter != null) {
+            if (bluetoothAdapter?.isEnabled == false) {
+                bluetoothOn()
+            }
+        } else {
+            // 블루투스를 지원하지 않는 경우
+            Log.d("bluetoothAdapter","Device doesn't support Bluetooth")
+            // TODO : 블루투스를 지원하지 않는 기기는 사용이 불가하다는 UI 필요
+        }
+        ///////////////////////////////////
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -31,5 +53,21 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    private val PERMISSIONS_BT = arrayOf(Manifest.permission.BLUETOOTH_SCAN, Manifest.permission.BLUETOOTH_CONNECT, Manifest.permission.BLUETOOTH_ADVERTISE)
+    private val REQUEST_ENABLE_BT=1
+    private val REQUEST_ALL_PERMISSION= 2
+
+    private val bluetoothAdapter: BluetoothAdapter? by lazy(LazyThreadSafetyMode.NONE) {
+        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        bluetoothManager.adapter
+    }
+
+
+    @SuppressLint("MissingPermission")
+    fun bluetoothOn(){
+        val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+        startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
     }
 }
