f35e1f4e8a4ba2b123cce0485e29bcb8231fc412
yogurt05j <yogurt05j@gmail.com>
2023-03-21 13:47:23 +0900

    ✨ feat : [SV] handle logo detection POST request

--- server/src/main/java/com/vd5/beyondb/controller/ApiController.java
+++ server/src/main/java/com/vd5/beyondb/controller/ApiController.java
@@ -3,6 +3,7 @@ package com.vd5.beyondb.controller;
 import com.vd5.beyondb.model.dto.request.CaptureDto;
 import com.vd5.beyondb.model.dto.request.CaptureMlDto;
 import com.vd5.beyondb.model.dto.response.CaptionDto;
+import com.vd5.beyondb.model.dto.response.DetectDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,16 +13,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @RestController
 @RequestMapping("api")
 public class ApiController {
+    String baseUrl = "http://70.12.130.121:5000/";      // ML server URL
+
     @PostMapping(path="/caption")
     public ResponseEntity<CaptionDto> captionImage(@RequestBody CaptureDto captureDto){
-        String baseUrl = "http://70.12.130.121:5000/";      // ML server URL
         CaptureMlDto captureMlDto = new CaptureMlDto(captureDto.getImgPath());  // request dto to ML server
         RestTemplate restTemplate = new RestTemplate();
         String result = restTemplate.postForObject(baseUrl + "s3/imagecaption", captureMlDto, String.class);
         CaptionDto captionDto = new CaptionDto(result);
         return new ResponseEntity<>(captionDto, HttpStatus.OK);
+    }
+
+    @PostMapping(path = "/program")
+    public ResponseEntity<DetectDto> detectLogo(@RequestBody CaptureDto captureDto){
+        log.info("===== detectLogo() =====");
+        CaptureMlDto captureMlDto = new CaptureMlDto(captureDto.getImgPath());  // request dto to ML server
+        RestTemplate restTemplate = new RestTemplate();
+        int programId = Integer.parseInt(restTemplate.postForObject(baseUrl + "s3/logodetect", captureMlDto, String.class));
+        String programName = programId + "번에 해당하는 프로그램 이름";       // get program name from DB by program id
+        DetectDto detectDto = new DetectDto(programId, programName);
+        return new ResponseEntity<>(detectDto, HttpStatus.OK);
     }
 }
