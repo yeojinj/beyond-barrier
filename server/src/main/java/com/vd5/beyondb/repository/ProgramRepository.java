c7fb6dc871bf9aa6f71dd65b026eb3b451f9f163
yogurt05j <yogurt05j@gmail.com>
2023-03-21 14:47:22 +0900

    ✨ feat : [SV] find program by id from DB

--- server/src/main/java/com/vd5/beyondb/repository/ProgramRepository.java
+++ server/src/main/java/com/vd5/beyondb/repository/ProgramRepository.java
@@ -1,8 +1,10 @@
 package com.vd5.beyondb.repository;
 
 import com.vd5.beyondb.model.Program;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProgramRepository extends JpaRepository<Program, Integer> {
 
+    Optional<Program> findById(int id);
 }
--- server/src/main/java/com/vd5/beyondb/service/ProgramService.java
+++ server/src/main/java/com/vd5/beyondb/service/ProgramService.java
@@ -1,8 +1,20 @@
 package com.vd5.beyondb.service;
 
+import com.vd5.beyondb.model.Program;
+import com.vd5.beyondb.repository.ProgramRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProgramService {
+
+    @Autowired
+    private ProgramRepository programRepository;
+
+    public Program findById(int id) {
+        programRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("DB에 존재하지 않는 프로그램 ID 입니다"));
+        return programRepository.findById(id).get();
+    }
 
 }
