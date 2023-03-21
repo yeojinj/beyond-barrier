48a448f907d894a46a7264c0d6adaae02ade655a
yogurt05j <yogurt05j@gmail.com>
2023-03-21 13:46:39 +0900

    ✨ feat : [SV] create logo detection response dto to tv

---
+++ server/src/main/java/com/vd5/beyondb/model/dto/response/DetectDto.java
@@ -0,0 +1,12 @@
+package com.vd5.beyondb.model.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class DetectDto {
+
+    private int programId;
+    private String programName;
+}
