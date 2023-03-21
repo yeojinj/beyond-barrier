bad243db78961c48447f9f6760a9bb8609e5aeb4
yogurt05j <yogurt05j@gmail.com>
2023-03-21 14:42:09 +0900

    ✨ feat : [SV] create program repository layer

---
+++ server/src/main/java/com/vd5/beyondb/repository/ProgramRepository.java
@@ -0,0 +1,8 @@
+package com.vd5.beyondb.repository;
+
+import com.vd5.beyondb.model.Program;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProgramRepository extends JpaRepository<Program, Integer> {
+
+}
