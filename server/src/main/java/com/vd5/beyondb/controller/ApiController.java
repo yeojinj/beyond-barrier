49b95976cf60be7d9fdddeda55e63517e90a2ef0
yogurt05j <yogurt05j@gmail.com>
2023-03-24 14:59:59 +0900

    ✨ feat : [SV] crawling program description and cast members

--- server/src/main/java/com/vd5/beyondb/controller/ApiController.java
+++ server/src/main/java/com/vd5/beyondb/controller/ApiController.java
@@ -6,7 +6,11 @@ import com.vd5.beyondb.model.dto.request.CaptureMlDto;
 import com.vd5.beyondb.model.dto.response.CaptionDto;
 import com.vd5.beyondb.model.dto.response.DetectDto;
 import com.vd5.beyondb.service.ProgramService;
+import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -45,5 +49,28 @@ public class ApiController {
         Program program = programService.findById(programId);       // get program name from DB by program id
         DetectDto detectDto = new DetectDto(program.getId(), program.getName());
         return new ResponseEntity<>(detectDto, HttpStatus.OK);
+    }
+
+    @PostMapping(path = "/program/crawling")
+    public String crawlingProgram(){
+        log.info("===== crawlingProgram() =====");
+        Program program = programService.findById(590);
+        String programName = program.getName().replace(" ", "+");   // handle spacing
+        log.info("프로그램명 : " + programName);
+        try {
+            Document rawData = Jsoup.connect(crawlingBaseUrl + programName).timeout(5000).get();
+            Elements descElem = rawData.select("div.detail_info span.desc._text");
+            String desc = descElem.get(0).text();
+            log.info("[크롤링] 설명 : " + desc);
+            Document rawCastData = Jsoup.connect(crawlingBaseUrl + programName + "+출연진").timeout(5000).get();
+            Elements castElem = rawCastData.select("div.item div.title_box strong");
+            for(int i = 0; i < castElem.size(); i++){
+                String cast = castElem.get(i).text();
+                log.info("[크롤링] 출연진 " + (i + 1) + " : " + cast);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return "크롤링 완료";
     }
 }
