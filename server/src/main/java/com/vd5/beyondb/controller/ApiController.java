a39ef705dbb3e6a2af527ff23991531ebabd198b
yogurt05j <yogurt05j@gmail.com>
2023-03-24 16:36:32 +0900

    🎨 style : [SV] code formatting

--- server/src/main/java/com/vd5/beyondb/controller/ApiController.java
+++ server/src/main/java/com/vd5/beyondb/controller/ApiController.java
@@ -31,28 +31,33 @@ public class ApiController {
     String mlBaseUrl = "http://70.12.130.121:5000/";      // ML server URL
     String crawlingBaseUrl = "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=";        // Naver search URL
 
-    @PostMapping(path="/caption")
-    public ResponseEntity<CaptionDto> captionImage(@RequestBody CaptureDto captureDto){
-        CaptureMlDto captureMlDto = new CaptureMlDto(captureDto.getImgPath());  // request dto to ML server
+    @PostMapping(path = "/caption")
+    public ResponseEntity<CaptionDto> captionImage(@RequestBody CaptureDto captureDto) {
+        CaptureMlDto captureMlDto = new CaptureMlDto(
+            captureDto.getImgPath());  // request dto to ML server
         RestTemplate restTemplate = new RestTemplate();
-        String result = restTemplate.postForObject(mlBaseUrl + "s3/imagecaption", captureMlDto, String.class);
+        String result = restTemplate.postForObject(mlBaseUrl + "s3/imagecaption", captureMlDto,
+            String.class);
         CaptionDto captionDto = new CaptionDto(result);
         return new ResponseEntity<>(captionDto, HttpStatus.OK);
     }
 
     @PostMapping(path = "/program")
-    public ResponseEntity<DetectDto> detectLogo(@RequestBody CaptureDto captureDto){
+    public ResponseEntity<DetectDto> detectLogo(@RequestBody CaptureDto captureDto) {
         log.info("===== detectLogo() =====");
-        CaptureMlDto captureMlDto = new CaptureMlDto(captureDto.getImgPath());  // request dto to ML server
+        CaptureMlDto captureMlDto = new CaptureMlDto(
+            captureDto.getImgPath());  // request dto to ML server
         RestTemplate restTemplate = new RestTemplate();
-        int programId = Integer.parseInt(restTemplate.postForObject(mlBaseUrl + "s3/logodetect", captureMlDto, String.class));
-        Program program = programService.findById(programId);       // get program name from DB by program id
+        int programId = Integer.parseInt(
+            restTemplate.postForObject(mlBaseUrl + "s3/logodetect", captureMlDto, String.class));
+        Program program = programService.findById(
+            programId);       // get program name from DB by program id
         DetectDto detectDto = new DetectDto(program.getId(), program.getName());
         return new ResponseEntity<>(detectDto, HttpStatus.OK);
     }
 
     @PostMapping(path = "/program/crawling")
-    public String crawlingProgram(){
+    public String crawlingProgram() {
         log.info("===== crawlingProgram() =====");
         Program program = programService.findById(590);
         String programName = program.getName().replace(" ", "+");   // handle spacing
@@ -62,9 +67,10 @@ public class ApiController {
             Elements descElem = rawData.select("div.detail_info span.desc._text");
             String desc = descElem.get(0).text();
             log.info("[크롤링] 설명 : " + desc);
-            Document rawCastData = Jsoup.connect(crawlingBaseUrl + programName + "+출연진").timeout(5000).get();
+            Document rawCastData = Jsoup.connect(crawlingBaseUrl + programName + "+출연진")
+                .timeout(5000).get();
             Elements castElem = rawCastData.select("div.item div.title_box strong");
-            for(int i = 0; i < castElem.size(); i++){
+            for (int i = 0; i < castElem.size(); i++) {
                 String cast = castElem.get(i).text();
                 log.info("[크롤링] 출연진 " + (i + 1) + " : " + cast);
             }
