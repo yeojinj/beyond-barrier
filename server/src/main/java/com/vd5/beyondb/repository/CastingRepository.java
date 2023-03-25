09e085d3296796b42478677e687a6ca39f7039d4
yogurt05j <yogurt05j@gmail.com>
2023-03-26 00:02:46 +0900

    ✨ feat : [SV] find casting list by program from DB

--- server/src/main/java/com/vd5/beyondb/repository/CastingRepository.java
+++ server/src/main/java/com/vd5/beyondb/repository/CastingRepository.java
@@ -1,8 +1,12 @@
 package com.vd5.beyondb.repository;
 
 import com.vd5.beyondb.model.Casting;
+import com.vd5.beyondb.model.Program;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CastingRepository extends JpaRepository<Casting, Long> {
 
+    List<Casting> findByProgram(Program program);
 }
+
--- server/src/main/java/com/vd5/beyondb/service/CastingService.java
+++ server/src/main/java/com/vd5/beyondb/service/CastingService.java
@@ -1,8 +1,22 @@
 package com.vd5.beyondb.service;
 
+import com.vd5.beyondb.model.Casting;
+import com.vd5.beyondb.model.Program;
+import com.vd5.beyondb.repository.CastingRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class CastingService {
+
+    @Autowired
+    private CastingRepository castingRepository;
+
+    public List<Casting> findByProgram(Program program) {
+        return castingRepository.findByProgram(program);
+    }
 
 }
