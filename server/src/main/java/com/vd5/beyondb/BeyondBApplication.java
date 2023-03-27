2c4c55100ffd1de1a6e0dd4ce4985a91dc630379
yogurt05j <yogurt05j@gmail.com>
2023-03-27 15:57:09 +0900

    ✨ feat : [SV] add annotation to enable auditing in JPA

--- server/src/main/java/com/vd5/beyondb/BeyondBApplication.java
+++ server/src/main/java/com/vd5/beyondb/BeyondBApplication.java
@@ -2,7 +2,9 @@ package com.vd5.beyondb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BeyondBApplication {
 
