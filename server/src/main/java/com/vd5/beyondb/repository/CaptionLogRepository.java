c9edb54e8418b21e9d12dcdd7dc6a7f8d97459a0
yogurt05j <yogurt05j@gmail.com>
2023-03-28 11:45:01 +0900

    ✨ feat : [SV] find caption log by id from DB

--- server/src/main/java/com/vd5/beyondb/repository/CaptionLogRepository.java
+++ server/src/main/java/com/vd5/beyondb/repository/CaptionLogRepository.java
@@ -1,8 +1,10 @@
 package com.vd5.beyondb.repository;
 
 import com.vd5.beyondb.model.CaptionLog;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CaptionLogRepository extends JpaRepository<CaptionLog, Long> {
 
+    Optional<CaptionLog> findCaptionLogById(Long id);
 }
