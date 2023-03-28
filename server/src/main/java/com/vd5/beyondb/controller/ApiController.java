5337ce36a8637ed50140484774a3de18636fc617
yogurt05j <yogurt05j@gmail.com>
2023-03-28 09:47:58 +0900

    ⚡ update : [SV] return caption log id instead of dto

--- server/src/main/java/com/vd5/beyondb/controller/ApiController.java
+++ server/src/main/java/com/vd5/beyondb/controller/ApiController.java
@@ -4,7 +4,6 @@ import com.vd5.beyondb.model.Program;
 import com.vd5.beyondb.model.ProgramLog;
 import com.vd5.beyondb.model.dto.request.CaptureDto;
 import com.vd5.beyondb.model.dto.request.CaptureMlDto;
-import com.vd5.beyondb.model.dto.response.CaptionDto;
 import com.vd5.beyondb.model.dto.response.DetectDto;
 import com.vd5.beyondb.service.CaptionLogService;
 import com.vd5.beyondb.service.CastingService;
@@ -33,6 +32,9 @@ import org.springframework.web.client.RestTemplate;
 public class ApiController {
 
     @Autowired
+    private CaptionLogService captionLogService;
+
+    @Autowired
     private ProgramLogService programLogService;
 
     @Autowired
@@ -45,14 +47,15 @@ public class ApiController {
     String crawlingBaseUrl = "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=";        // Naver search URL
 
     @PostMapping(path = "/caption")
-    public ResponseEntity<CaptionDto> captionImage(@RequestBody CaptureDto captureDto) {
+    public String captionImage(@RequestBody CaptureDto captureDto) {
+        log.info("===== captionImage() =====");
         CaptureMlDto captureMlDto = new CaptureMlDto(
             captureDto.getImgPath());  // request dto to ML server
         RestTemplate restTemplate = new RestTemplate();
         String result = restTemplate.postForObject(mlBaseUrl + "s3/imagecaption", captureMlDto,
             String.class);
-        CaptionDto captionDto = new CaptionDto(result);
-        return new ResponseEntity<>(captionDto, HttpStatus.OK);
+        Long logId = captionLogService.addCaptionLog(result);
+        return Long.toString(logId);
     }
 
     @PostMapping(path = "/program")
@@ -63,8 +66,7 @@ public class ApiController {
         RestTemplate restTemplate = new RestTemplate();
         int programId = Integer.parseInt(
             restTemplate.postForObject(mlBaseUrl + "s3/logodetect", captureMlDto, String.class));
-        Program program = programService.findById(
-            programId);       // get program name from DB by program id
+        Program program = programService.findById(programId);
         Long logId = programLogService.addProgramLog(program);
         return Long.toString(logId);
     }
